d15dd08dbc1cca4e01becb702535cef18b814e9d
Irene R <irene@smallwondermedia.com>
2019-02-11 11:53:18 -0500

    state

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+
+    state = {
+        names: ['JAKE', 'AMY', 'GINA', 'ROSA', 'CHARLES', 'TERRY', 'HOLT'],
+        targetName: "",
+        remainGuess: ""
+    }
+
+    componentDidMount() {
+        let targetName = this.state.names[Math.floor(Math.random()*this.state.names.length)];
+            this.setState({
+                targetName: targetName,
+            });
+            console.log(this.state.targetName);
+    }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+
+
+
       </div>
     );
   }
