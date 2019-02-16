688256e09e8958a04c015dcd5b0ad1ef88793a93
Irene R <irene@smallwondermedia.com>
2019-02-15 23:01:39 -0500

    added Reward comp

---
+++ src/Reward/Reward.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import AMY from "./images/amy.jpg";
+import CHARLES from "./images/charles.jpg";
+import GINA from "./images/gina.jpg";
+import HOLT from "./images/holt.jpg";
+import JAKE from "./images/jake.jpg";
+import ROSA from "./images/rosa.jpg";
+import TERRY from "./images/terry.jpg";
+
+const Reward = (props) => (
+
+    // targetName = imgName
+
+
+
+);
+
+
+export default Reward;
