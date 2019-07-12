5cd58ca7cf89c060e5fdc916dde376457d4da433
Irene Rojas <irene@smallwondermedia.com>
2019-07-11 23:15:10 -0400

    Updates

---
+++ precache-manifest.9e08247cec3dab28f82fcb5dc7e12804.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = [
+  {
+    "revision": "611412db035704573ffacc0031d370ab",
+    "url": "/brooklyn-react/static/media/charles.611412db.jpg"
+  },
+  {
+    "revision": "8505ccadd932869f2401",
+    "url": "/brooklyn-react/static/css/main.0e1e2aa3.chunk.css"
+  },
+  {
+    "revision": "97a4c0ffcff79d87da0b",
+    "url": "/brooklyn-react/static/js/1.97a4c0ff.chunk.js"
+  },
+  {
+    "revision": "efdbc27500e133eddfc1",
+    "url": "/brooklyn-react/static/js/runtime~main.efdbc275.js"
+  },
+  {
+    "revision": "9f1758bc03a0f8561eaf2f7ce9af48f7",
+    "url": "/brooklyn-react/static/media/b99_logo.9f1758bc.png"
+  },
+  {
+    "revision": "a6857bd7ba732486a17aff2cd4f88f89",
+    "url": "/brooklyn-react/static/media/amy.a6857bd7.jpg"
+  },
+  {
+    "revision": "8505ccadd932869f2401",
+    "url": "/brooklyn-react/static/js/main.8505ccad.chunk.js"
+  },
+  {
+    "revision": "8ebcbe06922e54950e6d76eb0dc13a66",
+    "url": "/brooklyn-react/static/media/gina.8ebcbe06.jpg"
+  },
+  {
+    "revision": "9d20d3940637fe9028735feb97683bb5",
+    "url": "/brooklyn-react/static/media/holt.9d20d394.jpg"
+  },
+  {
+    "revision": "63c8feba47f940acc7ed064d6a4408f6",
+    "url": "/brooklyn-react/static/media/jake.63c8feba.jpg"
+  },
+  {
+    "revision": "1abcd6062dac59a6dc0485b7e344bc39",
+    "url": "/brooklyn-react/static/media/rosa.1abcd606.jpg"
+  },
+  {
+    "revision": "d49e04e1d8ea95922eade87f310a5d22",
+    "url": "/brooklyn-react/static/media/terry.d49e04e1.jpg"
+  },
+  {
+    "revision": "09821d4545c3ddb240e43edd70725d54",
+    "url": "/brooklyn-react/index.html"
+  }
+];
